9b78f3d0dc7afc80f617fc503eeadc96bd323438
Mauro Juliano <43898163+MauroJuliano@users.noreply.github.com>
2021-08-02 19:21:53 -0300

    Initial Commit

---
+++ ReactView/ReactView.h
@@ -0,0 +1,18 @@
+//
+//  ReactView.h
+//  ReactView
+//
+//  Created by Mauro G Figueiredo on 02/08/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ReactView.
+FOUNDATION_EXPORT double ReactViewVersionNumber;
+
+//! Project version string for ReactView.
+FOUNDATION_EXPORT const unsigned char ReactViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactView/PublicHeader.h>
+
+
